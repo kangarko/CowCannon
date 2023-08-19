992611429d2a1bd6241d28506920af2ea9b5f0c1
kangarko <matej@matejpacan.com>
2023-08-19 16:56:00 +0200

    Ep34. Pathfinders and AI

--- src/main/java/org/mineacademy/cowcannon/command/PsychoCommand.java
+++ src/main/java/org/mineacademy/cowcannon/command/PsychoCommand.java
@@ -1,15 +1,19 @@
 package org.mineacademy.cowcannon.command;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Chicken;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.mineacademy.cowcannon.nms.AggressiveChicken1_20;
 
 public final class PsychoCommand implements CommandExecutor {
 
 	@Override
-	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
 		if (!(sender instanceof Player)) {
 			sender.sendMessage("Only players can use this command!");
 
@@ -19,8 +23,26 @@ public final class PsychoCommand implements CommandExecutor {
 		Player player = (Player) sender;
 
 		// TODO
-		player.sendMessage("Uncomment PsychoMob1_XX class and pom.xml remapped sections to make the command work");
+		//player.sendMessage("Uncomment PsychoMob1_XX class and pom.xml remapped sections to make the command work");
 		//new PsychoMob1_20(player.getLocation());
+
+		// /psycho goto <player>
+		if (args.length == 2 && "goto".equals(args[0])) {
+			Player targetPlayer = player.getServer().getPlayer(args[1]);
+
+			for (Entity nearby : player.getNearbyEntities(20, 20, 20)) {
+				if (nearby.getPersistentDataContainer().has(AggressiveChicken1_20.KEY)) {
+					Chicken chicken = (Chicken) nearby;
+
+					chicken.getPathfinder().moveTo(targetPlayer);
+					chicken.setTarget(targetPlayer);
+
+					player.sendMessage(ChatColor.GOLD + "Chicken is now following " + targetPlayer.getName());
+					break;
+				}
+			}
+		} else
+			new AggressiveChicken1_20(player.getLocation()); // /psycho
 
 		return true;
 	}
